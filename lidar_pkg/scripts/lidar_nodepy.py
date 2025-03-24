0a7395ed16634b2ddf879beaa652ed0313456eda
luohaojie-tt <1614078523@qq.com>
2025-03-24 23:29:03 +0800

    机器人避障C++实现与python实现

--- lidar_pkg/scripts/lidar_nodepy.py
+++ lidar_pkg/scripts/lidar_nodepy.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 import rclpy
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
+
+count = 0
+vel_pub = None
 def lidar_collback(msg):
+    global vel_pub,count
+
     dist = msg.ranges[180]
     print("前方距离: %f",dist)
+    vel_cmd = Twist()
+    if count >0:
+        count = count - 1
+        return
+    if dist < 1.5:
+        vel_cmd.angular.z = 0.5
+        count = 50
+    else:
+        vel_cmd.linear.x = 0.5
+    vel_pub.publish(vel_cmd)
 
 if __name__ == '__main__':
     rclpy.init()
     node = rclpy.create_node('lidar_node_py')
     sub = node.create_subscription(LaserScan, 'scan', lidar_collback, 10)
+    vel_pub = node.create_publisher(Twist, '/cmd_vel', 10)
     rclpy.spin(node)
--- lidar_pkg/src/lidar_node.cpp
+++ lidar_pkg/src/lidar_node.cpp
@@ -1,6 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
-
+#include <geometry_msgs/msg/twist.hpp>
 /**
  * @brief 获取激光雷达扫描数据并打印前方距离信息。
  *
@@ -11,10 +11,31 @@
  *            其中 ranges 数组存储了每个角度的测距值。
  * @return int 返回值固定为 0，表示函数执行成功。
  */
+
+rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub;
+int count = 0;
 int getScanback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
 {
     // 打印激光雷达正前方的距离值（假设第180个角度为正前方）
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "前方距离: '%f' 米", msg->ranges[180]);
+    float dis = msg->ranges[180];
+    geometry_msgs::msg::Twist vel_cmd;
+    if (count > 0)
+    {
+        count--;
+        return 0;
+    }
+
+    if (dis < 1.5)
+    {
+        vel_cmd.angular.z = 0.5;
+        count = 50;
+    }
+    else
+    {
+        vel_cmd.linear.x = 0.5;
+    }
+    vel_pub->publish(vel_cmd);
     return 0;
 }
 
@@ -41,6 +62,8 @@ int main(int argc, char *argv[])
 
     // 订阅 "scan" 话题，接收激光雷达数据，并绑定回调函数 getScanback
     auto sub = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, getScanback);
+
+    vel_pub = node->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
 
     // 进入事件循环，持续处理回调函数
     rclcpp::spin(node);
